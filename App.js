4cc8c7eb1b5d204a8ec679258eb8bf2b8d9e7de7
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-12 00:50:24 +0530

    Complete the App component

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import ProfileTap from './components/ProfileTap';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='home' screenOptions={
+          {
+            headerStyle: { backgroundColor: '#4b0150' },
+            headerTintColor:'#ffff', 
+            headerTitleAlign:'center'
+        }}>
+          <Stack.Screen name='home' component={Home} options={{ title: "UoV Health Care" }} />
+          <Stack.Screen name='profiletap' component={ProfileTap} options={{ title: "Student Health Profile" }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
